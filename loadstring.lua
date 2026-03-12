91bd44c88867685388ec1d652c7be65aef2b99bf
aiind <111066512+aiind@users.noreply.github.com>
2026-03-12 21:54:22 +0800

    Update loadstring URL to new repository location

--- loadstring.lua
+++ loadstring.lua
@@ -1,2 +1,2 @@
 WHITELIST = {"lolwhenme"};
-loadstring(game:HttpGet("https://raw.githubusercontent.com/borthdayzz/roblox-vc-spotify/main/main.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/aiind/roblox-vc-spotify/refs/heads/main/main.lua"))()
